1bf02dfd85251b69492bf6f67043e73ef9a4ba58
leonely1990 <leonel.wow.1990@gmail.com>
2020-11-24 23:00:59 -0400

    Juego 24/11/2020

--- Proyectos/Juego/src/clases/Personaje.java
+++ Proyectos/Juego/src/clases/Personaje.java
@@ -5,31 +5,235 @@
  */
 package clases;
 
-/**
- *
- * @author leonel
+ /**
+ * Clase que crea personajes
+ * 
+ * @author Leonel 
+ * @version 0.01
  */
-public class Personaje {
-    private String nombre;
+public class Personaje
+{
+    // Atributos
+    private final String nombre;
+    private double vidaTotal;
+    public double vida;
+    private double manaTotal;
+    private double mana;
     private int nivel;
-    private int exp;
-    private String prof;
-    
-    public Personaje(String nombre, String prof)
+    private double exp;
+    private double ataqueMin;
+    private double ataqueMax;
+    private int posX;
+    private int posY;
+    private final char pinta;
+
+    /**
+     * Constructor de objetos de la clase Personaje.
+     * 
+     * El personaje comenzara con un nivel de 1 y exp 0, tambien tendra ataque min y max en 1
+     * su posicion en el mapa de juego sera (X=25, Y=25) la vida y el mana estaran en 100
+     */
+    public Personaje(String nombre)
     {
+        // initialise instance variables
         this.nombre = nombre;
-        this.nivel = 1;
-        this.exp = 0;
-        this.prof = prof;
+        vidaTotal = 100;
+        vida = vidaTotal;
+        manaTotal = 100;
+        mana = manaTotal;
+        ataqueMin = 1;
+        ataqueMax = 1;
+        posX = 25;
+        posY = 25;
+        exp = 0;
+        nivel = 1;
+        pinta = '*';
     }
     
-    public void expPersonaje(int exp)
+    /**
+     * Devuelve el nombre del personaje
+     */
+    public String getNombre()
     {
-        this.exp += exp;
-        if(this.exp >= 1000)
-        {
-            nivel += 1;
-            this.exp -= 1000;
+        return nombre;
+    }
+    
+    /**
+     * Devuelve la experiencia del personaje
+     */
+    public double getExp()
+    {
+        return exp;
+    }
+    
+    /**
+     * Devuelve el nivel del personaje
+     */
+    public int getNivel()
+    {
+        return nivel;
+    }
+    
+    /**
+     * Devuelve el total del ataque minimo del personaje
+     */
+    public double getAtaqueMin()
+    {
+        return ataqueMin;
+    }
+    
+    /**
+     * Devuelve el total del ataque maximo del personaje
+     */
+    public double getAtaqueMax()
+    {
+        return ataqueMax;
+    }
+    
+    /**
+     * Devuelve la vida total del personaje
+     */
+    private double getVidaTotal()
+    {
+        return vidaTotal;
+    }
+    
+    /**
+     * Devuelve el mana total del personaje
+     */
+    private double getManaTotal()
+    {
+        return manaTotal;
+    }
+    
+    /**
+     * Devuelve la vida actual del personaje
+     */
+    public double getVida()
+    {
+        return vida;
+    }
+    
+    /**
+     * Devuelve el mana actual del personaje
+     */
+    public double getMana()
+    {
+        return mana;
+    }
+    
+    /**
+     * Devuelve la posicion del personaje con respecto a X
+     */
+    public int getPosX()
+    {
+        return posX;
+    }
+    
+    /**
+     * Devuelve la posicion del personaje con respecto a Y
+     */
+    public int getPosY()
+    {
+        return posY;
+    }
+    
+    /**
+     * Este metodo resta 1 la posicion en Y del personaje, si la posicion llega a 0
+     * no continuara restando por ende el personaje no caminara hacia arriba el valor
+     * de posY sera 0
+     */
+    public int CaminarArriba()
+    {
+        --posY;
+        if(posY <= 0){
+            posY = 0;
         }
+        return posY;
+    }
+    
+    /**
+     * Este metodo acepta una parametro de tipo entero el cual sera restado a la posicion del personaje,
+     * si la cantidad a restar hace que posY sea menos a 0 automaticamente posY sera 0
+     */
+    public int CaminarArriba(int arriba)
+    {
+        posY -= arriba;
+        if(posY < 0){
+           posY = 0;
+        }
+        return posY;
+    }
+    
+    public int CaminarAbajo()
+    {
+        ++posY;
+        if(posY > 50){
+            posY = 50;
+        }
+        return posY;
+    }
+    
+    public int CaminarAbajo(int abajo)
+    {
+        posY += abajo;
+        if(posY > 50){
+            posY = 50;
+        }
+        return posY;
+    }
+    
+    public int CaminarDerecha()
+    {
+        ++posX;
+        if(posX > 50){
+            posX = 50;
+        }
+        return posX;
+    }
+    
+    public int CaminarDerecha(int derecha)
+    {
+        posX += derecha;
+        if(posX > 50){
+            posX = 50;
+        }
+        return posX;
+    }
+    
+    public int CaminarIzquierda()
+    {
+        --posX;
+        if(posX < 0){
+            posX = 0;
+        }
+        return posX;
+    }
+    
+    public int CaminarIzquierda(int izquierda)
+    {
+        posX -= izquierda;
+        if(posX < 0){
+            posX = 0;
+        }
+        return posX;
+    }
+    
+    public char getPinta()
+    {
+        return pinta;
+    }
+    
+    public double atacar()
+    {
+        double dano = Math.random() * (ataqueMax - ataqueMin) + ataqueMin;
+        return dano;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Personaje [Nombre: " + nombre + ", Nivel: " + nivel + ", Exp: " + exp + ", Vida: " + vida + "/" + vidaTotal + 
+        ", Mana: " + mana + "/" + manaTotal + "]";
     }
 }
--- Proyectos/Juego/src/juego/Juego.java
+++ Proyectos/Juego/src/juego/Juego.java
@@ -5,6 +5,9 @@
  */
 package juego;
 
+import clases.Personaje;
+import java.util.Scanner;
+
 /**
  *
  * @author leonel
@@ -16,6 +19,55 @@ public class Juego {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        Personaje p1 = new Personaje("Leonel");
+        
+        System.out.println(p1.toString());
+        Mapa(p1);
+    }
+    
+    static void Mapa(Personaje personaje){
+        Scanner teclado = new Scanner(System.in);
+        int op;
+        while(true){
+            char mapa[][] = new char[50][50];
+            int y = personaje.getPosX();
+            int x = personaje.getPosY();
+            // Meter personaje en el mapa
+            for(int i=0; i<mapa.length; i++){
+                for(int j=0; j<mapa.length; j++){
+                    mapa[i][j] = ' ';                    
+                }
+            }
+            mapa[x][y] = personaje.getPinta();
+            for(int i=0; i<mapa.length; i++){
+                for(int j=0; j<mapa.length; j++){
+                    System.out.print(mapa[i][j]);                    
+                }
+                System.out.println();
+            }
+            System.out.println("1.Subir - 2.Bajar - 3.Derecha - 4.Izquierda");
+            System.out.print("Sel:");
+            op = teclado.nextInt();
+            switch(op){
+                case 1:
+                    personaje.CaminarArriba();
+                    break;
+                case 2:
+                    personaje.CaminarAbajo();
+                    break;
+                case 3:
+                    personaje.CaminarDerecha();
+                    break;
+                case 4:
+                    personaje.CaminarIzquierda();
+                    break;
+                default:
+                    System.out.println("Seleccione Opcion correcta");
+                    break;
+                    
+            }
+        }
     }
     
 }
